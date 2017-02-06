40416c8c00c90fdbb16865e25a8e7e0a4862e96b
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-02-06 09:46:15 +0900

    プラグインファイル追加

---
+++ qa-plugin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+    Plugin Name: Error Handler
+    Plugin URI:
+    Plugin Update Check URI:
+    Plugin Description: Error handling to display messages.
+    Plugin Version: 1.0.0
+    Plugin Date: 2017-02-06
+    Plugin Author: 38qa.net
+    Plugin Author URI:
+    Plugin License: GPLv2
+    Plugin Minimum Question2Answer Version: 1.7
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+        header('Location: ../../');
+        exit;
+}
+
+define('ERROR_HANDLER_DIR', __DIR__);
+ 
+require_once(ERROR_HANDLER_DIR.'/qa-error-handler-function.php');
+
+
+
+/*
+    Omit PHP closing tag to help avoid accidental output
+*/
